5866c3c3033bc425cbbede80c4baeafbe46be424
Lucas Moreira dos Santos <lucasmdsandradina@outlook.com>
2022-07-27 13:32:10 +0000

    Adicionado parte sobre a empresa na home.

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -27,5 +27,24 @@
             <li data-slide="3"></li>
             <li data-slide="4"></li>
         </ul>
-    <section>
+    </section>
+
+    <section id="empresa">
+        <div id="empresa-text">
+            <h2>
+                <span>Conheça um</span>
+                um pouco mais sobre a gente
+            </h2>
+            <p>
+                A <b>Casa Nova Empreendimentos</b> é uma empresa que atua em diversas áreas, para citar algumas: serviços financeiros, dedetização, serviços ambientais, dentre outros (conheça cada um deles mais abaixo). E para <b>garantir a excelência</b> que nossos clientes merecem, contamos com profissionais especialistas em cada uma de nossas áreas de atuação. São engenheiros, profissionais com anos de experiência, e por aí vai. Para resumir, nosso objetivo é mostrar que <b>MESMO QUE SE FAÇA MUITAS COISAS, É POSSÍVEL FAZER TUDO COM PERFEIÇÃO.</b>
+            </p>
+            <a href="#segmentos" class="link-button">
+                Nossos Segmentos
+                <i class="far fa-arrow-down" aria-hidden="true"></i>
+            </a>
+        </div>
+        <div id="empresa-image">
+            <img src="{{ asset('images/home/about.jpeg') }}" width="630" height="630" loading="lazy" alt="pessoas sentadas à mesa em reunião " />
+        </div>
+    </section>
 @endsection
